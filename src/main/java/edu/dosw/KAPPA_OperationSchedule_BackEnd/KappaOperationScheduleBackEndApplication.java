d5e9373f4f070b3dacedff88528805d37a01f962
Julian54628 <julian.arenas-a@mail.escuelaing.edu.co>
2025-11-12 20:21:36 -0500

    repositorio actualizado con nuevos cambios

---
+++ src/main/java/edu/dosw/KAPPA_OperationSchedule_BackEnd/KappaOperationScheduleBackEndApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.KAPPA_OperationSchedule_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaOperationScheduleBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappaOperationScheduleBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/KAPPA_OperationSchedule_BackEnd/KappaOperationScheduleBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.KAPPA_OperationSchedule_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaOperationScheduleBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
